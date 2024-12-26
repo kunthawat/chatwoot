fb2a747256c7e1afc9315c24607b42ba6495ba86
kunthawat <kunthawat@gmail.com>
2024-12-26 18:44:04 +0700

    Update production.rb
    Remove email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  # add smtp setting
-  smtp_settings = {
-  address: ENV.fetch('SMTP_ADDRESS', 'localhost'),
-  port: ENV.fetch('SMTP_PORT', 587)
-  }
-
-  smtp_settings[:authentication] = ENV.fetch('SMTP_AUTHENTICATION', 'login').to_sym if ENV['SMTP_AUTHENTICATION'].present?
-  smtp_settings[:domain] = ENV['SMTP_DOMAIN'] if ENV['SMTP_DOMAIN'].present?
-  smtp_settings[:user_name] = ENV.fetch('SMTP_USERNAME', nil)
-  smtp_settings[:password] = ENV.fetch('SMTP_PASSWORD', nil)
-  smtp_settings[:enable_starttls_auto] = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_ENABLE_STARTTLS_AUTO', true))
-  smtp_settings[:openssl_verify_mode] = ENV['SMTP_OPENSSL_VERIFY_MODE'] if ENV['SMTP_OPENSSL_VERIFY_MODE'].present?
-  smtp_settings[:ssl] = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_SSL', true)) if ENV['SMTP_SSL']
-  smtp_settings[:tls] = ActiveModel::Type::Boolean.new.cast(ENV.fetch('SMTP_TLS', true)) if ENV['SMTP_TLS']
-
-  config.action_mailer.delivery_method = :smtp unless Rails.env.test?
-  config.action_mailer.smtp_settings = smtp_settings
   
   # Code is not reloaded between requests.
   config.cache_classes = true
